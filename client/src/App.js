f5a0b7fc89ba7792fcb01b5e502e105dbc159977
SLuo5991 <sluo5991@gmail.com>
2022-11-04 20:20:53 -0400

    add react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
+import React from 'react';
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return (
-    <div className='App'>
-      <div>Hello World</div>
-    </div>
-  );
+  return <div className='App'>Hello</div>;
 }
 
 export default App;
