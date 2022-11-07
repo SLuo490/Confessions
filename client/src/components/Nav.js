68fa31ac24aca0d77d7edf7d1ebb02b4e63e739e
SLuo5991 <sluo5991@gmail.com>
2022-11-07 11:38:13 -0500

    add router link to nav bar

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,3 +1,11 @@
+import { Link } from 'react-router-dom';
+
 export default function Nav() {
-  return <nav className=''>Nav bar</nav>;
+  return (
+    <nav className=''>
+      <Link to='/'>
+        <button>Confessions</button>
+      </Link>
+    </nav>
+  );
 }
