00ad9ad2ce527bd087a7f58f660937812fe3b1d3
Shi Tao Luo <sluo5991@gmail.com>
2022-11-08 14:44:31 -0500

    edit nav bar

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,10 +1,13 @@
 import { Link } from 'react-router-dom';
+import '../pages/style.css';
 
 export default function Nav() {
   return (
-    <nav className=''>
+    <nav className='nav-bar bg-white'>
       <Link to='/'>
-        <button>Confessions</button>
+        <button type='button' className='btn color-celadon-blue'>
+          Confessions
+        </button>
       </Link>
     </nav>
   );
