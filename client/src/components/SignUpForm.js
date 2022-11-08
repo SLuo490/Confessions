bbcd889194744c53f035d0bbac3f5b2e5653bf6b
SLuo5991 <sluo5991@gmail.com>
2022-11-08 17:43:40 -0500

    change sign up form style and add generate random username functionality

--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -1,28 +1,47 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import '../pages/style.css';
 
 export default function Form() {
+  const [username, setUsername] = useState('');
+
+  const handleChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const generateRandomCharacterUsername = () => {
+    const characters =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let randomUsername = '';
+    let randomNum = Math.floor(Math.random() * 10) + 6;
+    for (let i = 0; i < randomNum; i++) {
+      randomUsername += characters.charAt(
+        Math.floor(Math.random() * characters.length)
+      );
+    }
+    setUsername(randomUsername);
+  };
+
   return (
     <div className='form-group'>
       <div className='username d-flex justify-content-center'>
-        <div className='w-37 margin-x'>
-          <label htmlFor='text'></label>
-          <input
-            type='text'
-            className='form-control form-control-lg py-3'
-            id='text'
-            placeholder='Username'
-          />
-        </div>
-        <div className='w-37 margin-x'>
-          <label htmlFor='button'></label>
-          <input
-            type='button'
-            className='form-control form-control-lg py-3 text-black btn-hover bg-color-celadon-blue text-white transition'
-            id='text'
-            value='Generate Username'
-          />
-        </div>
+        <label htmlFor='text'></label>
+        <input
+          type='text'
+          className='form-control form-control-lg py-3 w-50'
+          id='text'
+          placeholder={'Username'}
+          value={username}
+          onChange={handleChange}
+        />
+        <label htmlFor='button'></label>
+        <input
+          type='button'
+          className='button py-3 text-black btn-hover bg-color-celadon-blue text-white transition w-25 border-0'
+          id='button'
+          value='Generate Username'
+          onClick={generateRandomCharacterUsername}
+        />
       </div>
 
       <div className='w-75 input-center'>
