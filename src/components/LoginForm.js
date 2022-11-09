a64204ae696a2056aa6fc739433c11977bb39d6c
Shi Tao Luo <sluo5991@gmail.com>
2022-11-08 20:33:46 -0500

    implement login functionality

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,40 @@
 import { Link } from 'react-router-dom';
+import { auth } from '../utils/firebase';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { useState } from 'react';
 import '../pages/style.css';
 
 export default function Form() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSignIn = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          console.log(user);
+          alert("You're logged in!");
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorCode, errorMessage);
+          alert('Invalid email or password');
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className='form-group'>
       <div className='w-75 input-center'>
@@ -11,6 +44,7 @@ export default function Form() {
           className='form-control form-control-lg py-3'
           id='email'
           placeholder='Email Address'
+          onChange={handleChangeEmail}
         />
       </div>
       <div className='w-75 input-center'>
@@ -20,12 +54,14 @@ export default function Form() {
           className='form-control form-control-lg py-3'
           id='password'
           placeholder='Password'
+          onChange={handleChangePassword}
         />
       </div>
       <Link to='/home'>
         <button
           type='button'
           className='btn btn-primary w-75 input-center mt-4 py-2'
+          onClick={handleSignIn}
         >
           Log in
         </button>
