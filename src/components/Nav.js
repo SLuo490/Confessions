dfa7336f3ffcc41a9e331246f2a10f70ed4afdfb
SLuo5991 <sluo5991@gmail.com>
2022-11-09 13:22:46 -0500

    add log out functionality to nav bar and add create button

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,12 @@
 import { Link } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../utils/firebase';
+import { logOut } from '../utils/firebase';
 import '../pages/style.css';
 
 export default function Nav() {
+  const [user, loading] = useAuthState(auth);
+
   return (
     <nav className='nav-bar bg-white py-2'>
       <div className='container-fluid'>
@@ -14,18 +19,35 @@ export default function Nav() {
               Confessions
             </button>
           </Link>
-          <div className='auth'>
-            <Link to='/login' className='px-2'>
-              <button type='button' className='btn btn-outline-success'>
-                Login
+          {!user ? (
+            <div className='auth'>
+              <Link to='/login' className='px-2'>
+                <button type='button' className='btn btn-outline-success'>
+                  Login
+                </button>
+              </Link>
+              <Link to='/register'>
+                <button type='button' className='btn btn-outline-primary'>
+                  Sign Up
+                </button>
+              </Link>
+            </div>
+          ) : (
+            <div>
+              <Link to='/create'>
+                <button type='button' className='btn btn-outline-primary me-3'>
+                  Create
+                </button>
+              </Link>
+              <button
+                type='button'
+                className='btn btn-outline-danger'
+                onClick={logOut}
+              >
+                Log Out
               </button>
-            </Link>
-            <Link to='/register'>
-              <button type='button' className='btn btn-outline-primary'>
-                Sign Up
-              </button>
-            </Link>
-          </div>
+            </div>
+          )}
         </div>
       </div>
     </nav>
