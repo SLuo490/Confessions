6c9bfadb1688de6bfe8845de39be3b74483e28f4
Shi Tao Luo <sluo5991@gmail.com>
2022-11-22 16:56:10 -0500

    style post form and add state

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,53 +1,43 @@
 import { auth, db } from '../utils/firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { collection, addDoc } from 'firebase/firestore';
 import { ErrorAlert } from './index';
 
 export default function PostForm() {
-  const [user] = useAuthState(auth);
-  const [post, setPost] = useState('');
-  const [error, setError] = useState(null);
-
-  const handleSubmit = async (e) => {
-    if (post.length < 1) {
-      setError('Post cannot be empty');
-      return;
-    }
-    if (post.length > 280) {
-      setError('Post cannot be more than 280 characters');
-      return;
-    }
-    try {
-      await addDoc(collection(db, 'posts'), {
-        text: post,
-        createdAt: new Date(),
-        uid: user.uid,
-        name: user.displayName,
-      });
-    } catch (error) {
-      setError(error.message);
-    }
-  };
+  // state
+  const [post, setPost] = useState({ description: '', title: '' });
 
   return (
     <div className='PostForm'>
       <form>
-        {error && <ErrorAlert details={error} />}
-        <div className='form-group'>
+        {/* {error && <ErrorAlert details={error} />} */}
+        <div className='form-group mx-4 mt-4'>
+          <input
+            type='text'
+            className='form-control mb-3'
+            id='title'
+            placeholder='Enter title'
+            value={post.title}
+            onChange={(e) => setPost({ ...post, title: e.target.value })}
+          />
           <textarea
-            className='form-control'
+            className='form-control rounded'
             id='post'
-            rows='3'
+            rows='10'
             placeholder='What is on your mind?'
-            value={post}
-            onChange={(e) => setPost(e.target.value)}
+            value={post.description}
+            onChange={(e) => setPost({ ...post, description: e.target.value })}
           ></textarea>
+          {/* update letter count when user is typing */}
+          <div className='d-flex justify-content-end'>
+            <p className='text-muted'>{post.description.length}/600</p>
+          </div>
         </div>
         <button
           type='submit'
-          className='btn btn-primary'
-          onSubmit={handleSubmit}
+          className='btn btn-primary mt-2 mx-4 float-end w-25 py-2'
+          // onSubmit={handleSubmit}
         >
           Post
         </button>
