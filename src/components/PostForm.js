49c76d774855e300de93396b5de790574795746f
SLuo5991 <sluo5991@gmail.com>
2022-11-26 15:34:34 -0500

    add cancel button functionality

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -3,10 +3,18 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { useEffect, useState } from 'react';
 import { collection, addDoc } from 'firebase/firestore';
 import { ErrorAlert } from './index';
+import { useNavigate } from 'react-router-dom';
 
 export default function PostForm() {
   const [post, setPost] = useState({ title: '', description: '' });
   const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  // if user clicks cancel button, redirect to home page
+  const handleCancel = () => {
+    setPost({ title: '', description: '' });
+    navigate('/home');
+  };
 
   return (
     <div className='PostForm'>
@@ -31,15 +39,22 @@ export default function PostForm() {
           ></textarea>
           {/* update letter count when user is typing */}
           <div className='d-flex justify-content-end'>
-            <p className='text-muted'>{post.description.length}/600</p>
+            <p className='text-muted'>{post.description.length}/800</p>
           </div>
         </div>
         <button
           type='submit'
-          className='btn btn-primary mx-4 float-end w-25 py-2'
+          className='btn btn-primary mx-4 w-25 py-2'
           // onSubmit={handleSubmit}
         >
           Post
+        </button>
+        <button
+          type='submit'
+          className='btn btn-secondary mx-4 w-25 py-2'
+          onClick={handleCancel}
+        >
+          Cancel
         </button>
       </form>
     </div>
