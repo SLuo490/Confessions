850bf0502d1e0eb9607733a9cf0b1422ae774f4a
SLuo5991 <sluo5991@gmail.com>
2022-11-28 11:11:23 -0500

    remove edit page

--- src/components/Confession.js
+++ src/components/Confession.js
@@ -1,9 +1,9 @@
 export default function Confession({ children, ...props }) {
   return (
-    <div className='bg-white mt-5 p-3 rounded'>
+    <div className='bg-white mt-4 p-3 rounded'>
       <div>
-        <h5>{props.post.username}</h5>
-        <p>{props.post.title}</p>
+        <h6>{props.post.username}</h6>
+        <p>Title: {props.post.title}</p>
       </div>
       <div>
         <p>{props.post.description}</p>
