d89ab02efa8a2bc90dbcddd67eb06801f8ee56ce
SLuo5991 <sluo5991@gmail.com>
2022-12-02 18:46:54 -0500

    added some styling to home page

--- src/components/Confession.js
+++ src/components/Confession.js
@@ -2,8 +2,8 @@ export default function Confession({ children, ...props }) {
   return (
     <div className='bg-white mt-4 p-3 rounded'>
       <div>
+        <h2>Title: {props.post.title}</h2>
         <h6>{props.post.username}</h6>
-        <p>Title: {props.post.title}</p>
       </div>
       <div>
         <p>{props.post.description}</p>
