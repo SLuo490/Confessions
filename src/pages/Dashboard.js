1d141798b0523cee994e2d78cdb511cb3ec86b7b
SLuo5991 <sluo5991@gmail.com>
2022-12-02 18:56:40 -0500

    redirect user to login in dashboard if not logged in

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,25 @@
+import { Nav } from '../components';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { auth } from '../utils/firebase';
+
 export default function Dashboard() {
+  const [user, loading] = useAuthState(auth);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // if user not logged in, redirect to login page
+    const getData = async () => {
+      if (loading) return;
+      if (!user) return navigate('/login');
+    };
+    getData();
+  }, [user, loading, navigate]);
+
   return (
     <div>
+      <Nav />
       <h1>Dashboard</h1>
     </div>
   );
