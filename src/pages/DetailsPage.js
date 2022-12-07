ed8233753c0c2f075279221a65bd740e6e0fec38
SLuo5991 <sluo5991@gmail.com>
2022-12-07 14:33:13 -0500

    add comment to post

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -1,13 +1,14 @@
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { ErrorAlert, Confession, Nav } from '../components';
-import { doc, getDoc } from 'firebase/firestore';
+import { arrayUnion, doc, getDoc, updateDoc } from 'firebase/firestore';
 import { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { auth, db } from '../utils/firebase';
 
 export default function DetailsPage() {
   const [post, setPost] = useState({ title: '', description: '' });
-  const [allPost, setAllPost] = useState([]);
+  const [comment, setComment] = useState('');
+  const [allComment, setAllComment] = useState([]);
   const [error, setError] = useState('');
   const [user, loading] = useAuthState(auth);
 
@@ -16,6 +17,20 @@ export default function DetailsPage() {
   // get the post id from the url
   const location = useLocation();
   const id = location.pathname.split('/')[1];
+
+  // submit comment
+  const handleSubmit = async () => {
+    const docRef = doc(db, 'posts', id);
+    await updateDoc(docRef, {
+      comments: arrayUnion({
+        comment,
+        createdAt: new Date(),
+        user: user.uid,
+        username: user.displayName,
+      }),
+    });
+    setComment('');
+  };
 
   // pull post data from firestore
   useEffect(() => {
@@ -34,7 +49,30 @@ export default function DetailsPage() {
     <div>
       <Nav />
       <div className='container w-50'>
+        {error && <ErrorAlert message={error} />}
         <Confession post={post} />
+        <div>
+          <div className='d-flex'>
+            <input
+              type='text'
+              value={comment}
+              placeholder='Write a comment'
+              onChange={(e) => setComment(e.target.value)}
+              className='form-control py-2'
+            />
+            <button onClick={handleSubmit} className='btn btn-success'>
+              Submit
+            </button>
+          </div>
+          <div className='py-3'>
+            <h6>Comments</h6>
+            {allComment.map((comment) => (
+              <div>
+                <div></div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
