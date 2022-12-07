32cb3eb4722bce469695663a59d0f0d3dfc98382
SLuo5991 <sluo5991@gmail.com>
2022-12-07 14:17:58 -0500

    pull data from firestore to details page

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -1,15 +1,41 @@
-import { Confession, Nav } from '../components';
-import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { ErrorAlert, Confession, Nav } from '../components';
+import { doc, getDoc } from 'firebase/firestore';
+import { useState, useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { auth, db } from '../utils/firebase';
 
 export default function DetailsPage() {
+  const [post, setPost] = useState({ title: '', description: '' });
+  const [allPost, setAllPost] = useState([]);
+  const [error, setError] = useState('');
+  const [user, loading] = useAuthState(auth);
+
+  const navigate = useNavigate();
+
+  // get the post id from the url
   const location = useLocation();
+  const id = location.pathname.split('/')[1];
+
+  // pull post data from firestore
+  useEffect(() => {
+    const getData = async () => {
+      if (loading) return;
+      if (!user) return navigate('/login');
+
+      const docRef = await getDoc(doc(db, 'posts', id));
+      const docData = docRef.data();
+      setPost(docData);
+    };
+    getData();
+  }, [user, loading, navigate, id]);
 
   return (
     <div>
       <Nav />
-      <h1>Hello</h1>
+      <div className='container w-50'>
+        <Confession post={post} />
+      </div>
     </div>
   );
 }
