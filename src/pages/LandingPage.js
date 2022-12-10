4097e1301cb9aab582eb41f0c875c5c11bfe414f
SLuo5991 <sluo5991@gmail.com>
2022-12-10 09:21:33 -0500

    update landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,13 +1,6 @@
 import { Nav } from '../components';
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth } from '../utils/firebase';
 
 export default function LandingPage() {
-  const [user, loading] = useAuthState(auth);
-  const navigate = useNavigate();
-
   return (
     <div className='landingPage'>
       <Nav />
